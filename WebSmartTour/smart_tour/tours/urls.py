b862b66665a8be623f4edbb255c93825d185fdfa
NguyenDatcode <dat1272005no1st@gmail.com>
2026-01-03 19:43:31 +0700

    Add files via upload

---
+++ WebSmartTour/smart_tour/tours/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('roles', views.RoleView, basename='role')
+router.register('users', views.UserView, basename='user')
+router.register('services', views.ServiceView, basename='service')
+router.register('bookings', views.BookingView, basename='booking')
+router.register('booking-tours', views.BookingTourView, basename='booking-tour')
+router.register('booking-hotels', views.BookingHotelView, basename='booking-hotel')
+router.register('booking-transports', views.BookingTransportView, basename='booking-transport')
+router.register('invoices', views.InvoiceView, basename='invoice')
+router.register('reviews', views.ReviewView, basename='review')
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
